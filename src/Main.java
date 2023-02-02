81f32cb0126e8bf9ff1fe5935dd8baaf94b04f74
wlexa <w.lexandra@gmail..com>
2023-02-02 23:35:28 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author joanneRowling = new Author ("Rowling", "Joanne");
+        Book harryPotter = new Book ("Harry Potter", joanneRowling, 1995);
+        //распечатка с Автор = ссылка на объект
+        System.out.println("Harry Potter: " + harryPotter.getName() + " " +
+                harryPotter.getAuthor() + " " + harryPotter.getYear());
+        //распечатка в привычном виде
+        System.out.println("Harry Potter: " + harryPotter.getName() + " " + joanneRowling.getFirstName() + " " +
+                joanneRowling.getLastName() + " " + harryPotter.getYear());
+        //распечатка через метод ОК / не ОК ?
+        System.out.println("Harry Potter: " + harryPotter.getName() + " " +
+                harryPotter.authorToString(harryPotter.getAuthor()) + " " + harryPotter.getYear());
+
+        //изменяем год публикации
+        harryPotter.setYear(1997);
+        System.out.println("harryPotter.getYear() = " + harryPotter.getYear());
+
+        Author johnTolkien = new Author("Tolkien","John");
+        Book hobbit = new Book ("The Hobbit",johnTolkien, 1937);
+        System.out.println("The Hobbit: " + hobbit.getName() + " " +
+                hobbit.authorToString(hobbit.getAuthor()) + " " + hobbit.getYear());
+
+
+
     }
 }
